0e78b3d3524ef1bc319ab2369aad643e4a14cd10
yjwoo1 <yjwoo@goqual.com>
2022-11-05 17:49:26 +0900

    [와우][완전탐색] BJ2503 숫자야구 #1

---
+++ 와우/1주차/BJ2503_숫자야구.py
@@ -0,0 +1,26 @@
+import sys
+from itertools import permutations
+
+n=int(input())
+num=list(permutations([1,2,3,4,5,6,7,8,9], 3))
+
+for _ in range(n):
+    q, s, b = map(int, input().split())
+    q=list(str(q))
+    remove_cnt=0
+    
+    for i in range(len(num)):
+        s_cnt = b_cnt =0
+        i-=remove_cnt
+        for j in range(3):
+            q[j]=int(q[j])
+            if q[j] in num[i]:
+                if j==num[i].index(q[j]):
+                    s_cnt+=1
+                else:
+                    b_cnt+=1
+        if not(s==s_cnt and b==b_cnt):
+            num.remove(num[i])
+            remove_cnt+=1
+            
+print(len(num))
