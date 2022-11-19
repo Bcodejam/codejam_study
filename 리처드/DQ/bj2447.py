436d51ee41ec76ac13cbab6af7337c4d69ef853f
Gnadi4 <uno6070w@gmail.com>
2022-11-19 22:33:00 +0900

    3주차 끗! #3

--- 리처드/DQ/bj2447.py
+++ 리처드/DQ/bj2447.py
@@ -4,4 +4,25 @@ input = sys.stdin.readline
 
 n = int(input().rstrip())
 
-map = [[] * i for i in range(n)]
+map_n = [[' ' for i in range(n)] for j in range(n)]
+
+def func(l: int, x, y):
+    if l == 3:
+        for i in range(3):
+            for j in range(3):
+                if not(i == 1 and j == 1):
+                    map_n[x+i][y+j] = '*'
+        return
+    else:
+        div = l//3
+        for i in range(3):
+            for j in range(3):
+                if not(i == 1 and j == 1):
+                    func(div, x+(i*div), y+(j*div))
+
+func(n, 0, 0)
+
+for i in range(n):
+    for j in range(n):
+        print(map_n[i][j], end='')
+    print()
