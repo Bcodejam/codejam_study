57c7434e7053ce4fa4a4dc3a1e942f1ad9037719
22sonamu <bccard9503@bccard9503ui-Macmini.local>
2022-12-23 08:30:58 +0900

    [마라] 3078 좋은친구 - 시간초과 (저장용) #7

---
+++ 마라/7주차/3078_좋은친구.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+
+N, K = map(int, input().split())
+
+queueList = []
+
+#이름 길이는 최대 20이므로
+for _ in range(21) :
+    queueList.append(deque())
+
+answer = 0
+
+for record in range(N) :
+    nameLength = len(input())
+    if len(queueList[nameLength]) == 0 :
+        queueList[nameLength].append(record)
+    else :
+        if record - queueList[nameLength][-1] <= K :
+            for s in queueList[nameLength] :
+                if record - s <= K :
+                    answer += 1
+            queueList[nameLength].append(record)
+
+print(answer)
+
