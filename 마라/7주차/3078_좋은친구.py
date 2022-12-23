d78a02b9651ae003fc26c7299b33a06d44fece84
22sonamu <bccard9503@bccard9503ui-Macmini.local>
2022-12-23 10:46:09 +0900

    [마라] 3078 좋은친구 #7

--- 마라/7주차/3078_좋은친구.py
+++ 마라/7주차/3078_좋은친구.py
@@ -5,7 +5,7 @@ N, K = map(int, input().split())
 
 queueList = []
 
-#이름 길이는 최대 20이므로
+#이름 길이는 최대 20이므로 , index 가 길이인 배열 생성
 for _ in range(21) :
     queueList.append(deque())
 
@@ -16,11 +16,14 @@ for record in range(N) :
     if len(queueList[nameLength]) == 0 :
         queueList[nameLength].append(record)
     else :
-        if record - queueList[nameLength][-1] <= K :
-            while record - queueList[nameLength][0] > K :
-                queueList[nameLength].popleft()
-            answer += len(queueList[nameLength])
-            queueList[nameLength].append(record)
+        #큐가 비어있지 않고, 큐의 첫번째 원소와 등수차가 K 초과일 경우
+        while queueList[nameLength] and record - queueList[nameLength][0] > K :
+            #친한 친구가 아니므로 pop
+            queueList[nameLength].popleft()
+        #남아있는 애들은 친한친구임
+        answer += len(queueList[nameLength])
+        queueList[nameLength].append(record)
+            
 
 print(answer)
 
