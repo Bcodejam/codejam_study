0dd1621cedaca4404bbf711b05e8fcc12382d5bf
22sonamu <22sonamu@naver.com>
2023-01-23 23:12:57 +0900

    [마라] 1327 소트게임 #11

--- 마라/11주차/1327_소트게임.py
+++ 마라/11주차/1327_소트게임.py
@@ -1,33 +1,39 @@
-from  collections import deque;
+from collections import deque
 
-N , K = map(int, input().split())
+N, K = map(int, input().split())
+lst = list(map(str, input().split()))
+string = ''.join(lst)
+
+answer = sorted(lst)
+answerString = ''.join(answer)
 
 visited = set()
 
-lst = list(map(str , input().split()))
 
-answer = 100
-def bfs() :
-    global answer 
-    global lst
-    strlist = ''.join(lst)
-    q = deque()
-    q.append([strlist, 0])
-    visited.add(strlist)
-    while q :
-        num , depth = q.popleft()
-        if num == ''.join(sorted(lst)) :
-            answer = min(answer, depth)
-        if num not in visited :
-            visited.add(num)
-            for i in range(len(str(num)) - K) :
-                tmp = num[i:i+K][::-1]
-                for j in range(K) :
-                    num[j] = tmp[j]
-                q.append([num, depth+1])
-                
-bfs()
-print(answer)    
+#depth , 문자열을 저장
+q = deque([[ 0 , string]])
+
+while q :
+  depth , s = q.popleft()
+  #sort 된 문자열일경우
+  if s == answerString :
+    #깊이 출력하고 종료
+    print(depth)
+    exit()
+  
+
+  if s not in visited :
+    if N - K + 1 <= N :
+      for i in range(0 , N - K + 1) :
+        #i부터 K 길이만큼 문자열 뒤집기
+        j = i + K -1 
+        tmp = list(s)
+        tmp[i : j+1] = tmp[i : j+1][::-1]
+        s_new = ''.join(tmp)
+        q.append([depth + 1 , s_new])
+        visited.add(s)
+            
+print(-1)
 
        
     
